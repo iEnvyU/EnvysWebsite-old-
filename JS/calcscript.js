0921ef31d00342e36e98ca67eecbf7c573c1ef26
iEnvyU <69677619+iEnvyU@users.noreply.github.com>
2021-02-09 01:31:11 -0600

    Add files via upload

--- JS/calcscript.js
+++ JS/calcscript.js
@@ -1,8 +1,9 @@
-var CheckForVariable = false;
+//Data Field
 const variables = ['+', '-', '*', '/'];
-const numbers = ['0','1','2','3','4','5','6','7','8','9']
+const numbers = ['0','1','2','3','4','5','6','7','8','9'];
 
 function NumberButtons(val) {
+        //If Text Box Contains A Completed Equation Or An Error, Reset And Continue. Else Continue
         if(document.getElementById("txtBox").value.includes('=') || document.getElementById("txtBox").value.includes('E'))
                 document.getElementById("txtBox").value = "" + val;
         else 
@@ -10,62 +11,71 @@ function NumberButtons(val) {
 }
 
 function OperatorButtons(val) {
+        //If Equation Is Complete Do nothing
         if(document.getElementById("txtBox").value.includes('='))
                 return;
-
-        let z = document.getElementById("txtBox").value.slice(-1);
-
-        for(let item of numbers)
-                if(z.includes(item))
-                        document.getElementById("txtBox").value += val;
+        
+        //Checks If The Text Box Is Empty And If The Operator I Clicked Is A "-"
+        if(document.getElementById("txtBox").value == "" && val == "-")
+                document.getElementById("txtBox").value += val;
+                
+        //Function Call
+        CheckForNumber(val);
 }
 
 function DecimalButton(val) {
+        //If Text Box Contains A Completed Equation Or An Error, Reset And Continue.
         if(document.getElementById("txtBox").value.includes('=') || document.getElementById("txtBox").value.includes('E'))
                 document.getElementById("txtBox").value = "" + val;
 
-        let z = document.getElementById("txtBox").value.slice(-1);
-
-        for(let item of numbers)
-                if(z.includes(item))
-                        document.getElementById("txtBox").value += val;
+        //Function Call
+        CheckForNumber(val);
 }
 
 function OperationButton(val) {
-        
+        //Do Nothing If There's An Error Or If An Equation Was Made
         if(document.getElementById("txtBox").value == "Error" || document.getElementById("txtBox").value.includes('='))
                 return;
 
+        //Main Evaluation Of Equation
         let y = document.getElementById("txtBox").value;
         let z = eval(y);
 
+        //Error Checking For Divide By Zero (Infinity)
         if(z == "Infinity")
         {
                 document.getElementById("txtBox").value = "Error";
                 return;
         }
 
+        //Error Checking For Zero Divided By Zero
         if(y.includes("0/0"))
                 document.getElementById("txtBox").value = "Error";
-
-        for(let item of numbers)
-                if (document.getElementById("txtBox").value.includes(item))
-                        CheckForVariable = true;
-        
-        if(CheckForVariable === true)
-                document.getElementById("txtBox").value += val += z;
-        else
-                document.getElementById("txtBox").value = "Error";
-
-        CheckForVariable = false;
+       
+        //To Find If The Equation Contains A Variable then evaluates the Equation
+        for(let item of variables)
+                 if (document.getElementById("txtBox").value.includes(item))
+                        document.getElementById("txtBox").value = y + val + z;
 }
 
 function DelButton() {
+        //To Shorten A Variable
         const txtBox = document.getElementById("txtBox");
 
+        //If Text Box Length Is Is Greater Than 0 Go To The Last Index In The Text Box Substring And -1
         if(txtBox.value.length > 0)
                 txtBox.value = txtBox.value.substring(0, txtBox.value.length - 1);
 
+        //Reset Text Box if Equation Was Complete Or There Was An Error
         if(txtBox.value.includes('=') || txtBox.value.includes('E'))
                 txtBox.value = "";
+}
+
+function CheckForNumber(val) {
+        //Check Last Character For Numbers, If True Then Add An Operator
+        let z = document.getElementById("txtBox").value.slice(-1);
+
+        for(let item of numbers)
+                if(z.includes(item))
+                        document.getElementById("txtBox").value += val;
 }
